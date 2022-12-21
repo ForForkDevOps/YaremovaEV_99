6837cd7e5c9a6b48ccb495680d30966228ffedaf
ZB <yaremova10@mail.ru>
2022-12-21 18:00:28 +0300

    Done Calculator v1

---
+++ Calculator.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    char operation;
+    double firstNumber, secondNumber;
+
+    std::cout << "Введите первое число: "; std::cin >> firstNumber;
+    std::cout << "Введите второе число: "; std::cin >> secondNumber;
+    std::cout << "Введите операцию: "; std::cin >> operation;
+    if (operation == '/' && secondNumber == 0)
+    {
+        std::cout << "Ошибка!";
+    }
+    else
+    {
+        switch (operation)
+        {
+        case '+':
+            std::cout << "Ответ: " << firstNumber + secondNumber << std::endl;
+            break;
+        case '-':
+            std::cout << "Ответ: " << firstNumber - secondNumber << std::endl;
+            break;
+        case '*':
+            std::cout << "Ответ: " << firstNumber * secondNumber << std::endl;
+            break;
+        case '/':
+            std::cout << "Ответ: " << firstNumber / secondNumber << std::endl;
+            break;
+        default:
+            std::cout << "Ошибка!";
+        }
+    }
+    return 0;
+}
